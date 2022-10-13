a5a8212af9bc2ca906f77a30d2114faee26f372a
dochi486 <93mskim@gmail.com>
2022-10-13 19:40:51 +0900

    리액트 라우터, 파이어베이스 재설치

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import AppRouter from './Router';
+import { useState } from 'react';
+import { authService } from 'myBase';
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer> &copy; Dwitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
