5ea78147e4264fbe1f7a968f578751ce193f457f
dochi486 <93mskim@gmail.com>
2022-10-14 16:04:45 +0900

    사진 업로드 준비 중 일단 제출 시 파일 먼저 업로드 할 수 있도록 구조 변경

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,6 +12,7 @@ import Dweet from 'components/Dweet';
 const Home = ({ userObj }) => {
   const [dweet, setDweet] = useState('');
   const [dweets, setDweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   // const getDweets = async () => {
   //   const dbDweets = await dbService.collection('dweets').get();
@@ -40,12 +41,12 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection('dweets').add({
-      text: dweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setDweet('');
+    // await dbService.collection('dweets').add({
+    //   text: dweet,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    // });
+    // setDweet('');
   };
 
   const onChange = (event) => {
@@ -53,6 +54,25 @@ const Home = ({ userObj }) => {
       target: { value },
     } = event;
     setDweet(value);
+  };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0]; //오직 하나의 파일만 받기 때문에 0번째 인덱스에 접근
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClickClear = () => {
+    setAttachment(null);
   };
 
   return (
@@ -65,7 +85,14 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Dweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClickClear}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {dweets.map((dweet) => (
