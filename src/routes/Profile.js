f5e6722d23a03e54122e8e50d5a750a09c1df1e5
dochi486 <93mskim@gmail.com>
2022-10-14 13:01:46 +0900

    useHistory로 로그아웃 버튼 눌렀을 떄 홈으로 이동시킴

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,12 @@
 import { authService } from 'myBase';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Profile = () => {
+  const history = useHistory();
   const onClickLogOut = () => {
     authService.signOut();
+    history.push('/');
   };
   return (
     <>
