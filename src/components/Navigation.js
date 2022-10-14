f5fbb49cd3d5dff2347eaa308a2a23c0a08e7c80
dochi486 <93mskim@gmail.com>
2022-10-14 13:28:13 +0900

    return 추가하니까 잘 됨

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/profile">Profile</Link>
-      </li>
-    </ul>
-  </nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
