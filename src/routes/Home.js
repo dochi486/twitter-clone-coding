9776dbaf51cf12dea892a19593e554b0522710bc
dochi486 <93mskim@gmail.com>
2022-10-14 13:44:08 +0900

    firestore에 저장

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,40 @@
 import Navigation from 'components/Navigation';
-import React from 'react';
+import { dbService } from 'myBase';
+import React, { useState } from 'react';
 
-const Home = () => <div> Home</div>;
+const Home = () => {
+  const [dweet, setDweet] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dbService.collection('dweets').add({
+      dweet: dweet,
+      createdAt: Date.now(),
+    });
+    setDweet('');
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setDweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={dweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind"
+          maxLength={120}
+        />
+        <input type="submit" value="Dweet" />
+      </form>
+    </div>
+  );
+};
+
 export default Home;
